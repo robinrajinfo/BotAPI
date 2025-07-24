815fa514ac7b771d0e04d93cdd085c7866671949
Robin Raj <robinraj.personal@gmail.com>
2025-07-24 20:34:10 +0530

    testing api

--- bot/urls.py
+++ bot/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.bot_list, name='bot_list'),
+    path('', views.bot_list, name='bot_list'),               # HTML view
+    path('api/bots/', views.bot_list_api, name='bot_list_api'),  # API view
 ]
--- bot/views.py
+++ bot/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from .models import Bot
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import BotSerializer
 
-# Create your views here.
+# HTML View (for normal browser page)
 def bot_list(request):
     bots = Bot.objects.all()
     return render(request, 'bot/bot_list.html', {'bots': bots})
-    
+
+# API View (for /api/bots/)
+@api_view(['GET', 'POST'])
+def bot_list_api(request):
+    if request.method == 'GET':
+        bots = Bot.objects.all()
+        serializer = BotSerializer(bots, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = BotSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
